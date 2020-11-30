7fbd2728da70e3c01e8291913b0d7d9c393c0414
Chris <me@chrisconnelly.dev>
2020-11-30 13:43:20 -0700

    V 1.0.5

--- src/UI/TextInput.svelte
+++ src/UI/TextInput.svelte
@@ -1,10 +1,12 @@
 <script>
-  export let controlType;
+  export let controlType = null;
   export let id;
   export let label;
-  export let rows;
+  export let rows = null;
   export let value;
-  export let type;
+  export let type = "text";
+  export let valid = true;
+  export let validityMessage = "";
 </script>
 
 <style>
@@ -38,13 +40,26 @@
     width: 100%;
     margin: 0.25rem 0;
   }
+
+  .invalid {
+    border-color: red;
+    background: #fde3e3;
+  }
+
+  .error-message {
+    color: red;
+    margin: 0.25rem 0;
+  }
 </style>
 
 <div class="form-control">
   <label for={id}>{label}</label>
   {#if controlType === 'textarea'}
-    <textarea {rows} {id} {value} on:input />
+    <textarea class:invalid="{!valid}" {rows} {id} {value} on:input />
   {:else}
-    <input type="{type}" {id} {value} on:input />
+    <input class:invalid="{!valid}" {type} {id} {value} on:input />
+  {/if}
+  {#if validityMessage && !valid}
+    <p class="error-message">{validityMessage}</p>
   {/if}
 </div>
